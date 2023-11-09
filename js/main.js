d4cd402a4e9c1dab45e185941c346ef6836173ea
Kornelia Adabugday <seadabugday@gmail.com>
2023-11-09 09:36:32 +0100

    last touches

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ let searchBtn = document.getElementById('search-btn');
 let searchEpisodeBtnContainer = document.querySelector('.search-episode-btn-container');
 let searchContainer = document.querySelector('.search-container');
 let ul = document.getElementById('ul');
+let headerContainer = document.getElementById('header-container');
 let title;
 let episodeInput1 = document.getElementById('episodeInput1');
 let episodeInput2 = document.getElementById('episodeInput2');
-let btn3 = document.getElementById('btn3');
+let moreInfoBtn = document.getElementById('more-info-btn');
+let newSearchBtn = document.getElementById('new-search-btn');
 let showId;
 
 searchEpisodeBtnContainer.classList.add('hide');
@@ -16,7 +18,6 @@ inputLabel.value = "";
 
 });
 
-
 const movieLink = 'https://api.tvmaze.com/search/shows?';
 
 searchBtn.addEventListener('click', async function () {
@@ -25,13 +26,22 @@ searchBtn.addEventListener('click', async function () {
         const movieResponse = await fetch((movieLink + "q=" + inputLabel.value))
         console.log(movieLink + "q=" + inputLabel.value);
 
-
         if (movieResponse.ok === false) {
             throw new Error(`HTTP error code: ${movieResponse.status}, HTTP error message: ${movieResponse.statusText}`);
         }
 
         const movieData = await movieResponse.json();
 
+        episodeInput1.value = "..."
+        episodeInput1.addEventListener('focus', function () {
+            episodeInput1.value = "";
+            });
+
+        episodeInput2.value = "..."
+        episodeInput2.addEventListener('focus', function () {
+            episodeInput2.value = "";
+            });
+            
         let movieList = "";
 
         if (movieData.length === 0) {
@@ -52,6 +62,7 @@ searchBtn.addEventListener('click', async function () {
 
             searchEpisodeBtnContainer.classList.toggle('hide');
             searchContainer.classList.add('hide');
+            headerContainer.classList.add('hide');
         }
 
         ul.innerHTML = movieList;
@@ -62,7 +73,7 @@ searchBtn.addEventListener('click', async function () {
 });
 
 
-btn3.addEventListener('click', async function () {
+moreInfoBtn.addEventListener('click', async function () {
     try {
 
         const episodes = await fetch(`https://api.tvmaze.com/shows/${showId}/episodebynumber?season=${episodeInput1.value}&number=${episodeInput2.value}`);
@@ -79,18 +90,25 @@ btn3.addEventListener('click', async function () {
         const season = data.season
         const summary = data.summary
 
-        episodeList += `<li>${title}<br>Season ${episodeInput1.value} episode${episodeInput2.value} is called ${name}. <br><br><h4>A small summary about the episode:</h4>${summary}</li>`
+        episodeList += `<li><h3>${title}</h3>Season ${episodeInput1.value} episode${episodeInput2.value} is called ${name}. <br><h4>A small summary about the episode:</h4>${summary}</li>`
 
         ul.innerHTML = episodeList;
 
-        searchContainer.classList.toggle('hide');
         searchEpisodeBtnContainer.classList.toggle('hide');
-
+        newSearchBtn.classList.toggle('hide');
+        
     } catch (error) {
         console.log(error);
         alert('The season or episode does not exist, try another search!');
- 
-    }
-
+    } 
 });
 
+newSearchBtn.addEventListener('click', function() {
+    episodeList = ""; 
+    ul.innerHTML = "";
+    inputLabel.value = "...";
+
+    searchContainer.classList.toggle('hide');
+    headerContainer.classList.toggle('hide');
+    newSearchBtn.classList.toggle('hide');
+});
